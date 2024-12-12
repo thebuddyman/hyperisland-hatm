113bcfc853f46a6f9c9f48fac7d6866e27e4e6ee
Thomas Tjaja <thomas.tjaja@hyperisland.co.uk>
2024-12-12 18:54:54 +0100

    update the background header

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -42,7 +42,7 @@ export function ChatHeader({
   };
 
   return (
-    <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
+    <header className="flex sticky top-0 py-1.5 items-center px-2 md:px-2 gap-2">
       
       <SidebarToggle />
       {(!open || windowWidth < 768) && (
